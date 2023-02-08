42a6224309e4921d5580db3df10fdd8f484dcf70
JungIn <lego0520lwy@icloud.com>
2023-02-08 23:39:46 +0900

    tt

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <nuxt-link to="/test">test</nuxt-link>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
---
+++ pages/test.vue
@@ -0,0 +1,3 @@
+<template>
+    <nuxt-link to="/">home</nuxt-link>
+</template>
